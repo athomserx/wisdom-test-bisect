6b461fa3064b60b54cf64a2f6d497899268e39d7
Sergio Mancilla <serx.athom@gmail.com>
2026-01-08 15:54:31 -0500

    15th commit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def main():
   # Some line here
-  print('Program is working')
+  result = add_numbers(5, 8)
+  print(f"Some dumb result: {result}")
+
+
+def add_numbers(a, b):
+  return a + b
 
 main()
 
